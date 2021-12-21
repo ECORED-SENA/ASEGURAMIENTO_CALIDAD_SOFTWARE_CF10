2992b71a7ccba1fca84c3002519555d72c34945e
maronita <maronita@users.noreply.github.com>
2021-12-21 10:14:19 -0500

    Videos

--- src/views/tema1.vue
+++ src/views/tema1.vue
@@ -156,8 +156,7 @@
       .row.justify-content-center
         figure
           .video
-            iframe(width="560" height="315" src="https://www.youtube.com/embed/2L91WMqw96A" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen)
-
+            <iframe width="560" height="315" src="https://www.youtube.com/embed/XbtDHcIfTjY" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
 
 
 
